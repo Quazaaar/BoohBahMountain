44b456e3e270f5a7f520d9db26bac316488c9b53
Quazaaar <chuckcbird@gmail.com>
2025-01-22 08:00:44 -0800

    init commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+package BoohBahMountain;
+import UI.GameUI;
+
+class Main{
+    public static void main(String[] args) {
+        
+        GameUI GUI = new GameUI();
+        GUI.start();
+    }
+    
+}
